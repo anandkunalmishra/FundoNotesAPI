52ba25c2b1a897b596b65c0073653723b923c2b4
Anand Kunal Mishra <73222935+anandkunalmishra@users.noreply.github.com>
2024-02-24 13:38:32 +0530

    [Anand]: Interfaces_Services added to ReposLayer

--- FundoNotesAPI/Program.cs
+++ FundoNotesAPI/Program.cs
@@ -1,11 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Repository_Layer.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<FundoContext>(x => x.UseSqlServer(builder.Configuration["ConnectionStrings:FundoAppdb"]));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
---
+++ Repository_Layer/Services/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Repository_Layer.Services
+{
+	public class UserRepository
+	{
+		public UserRepository()
+		{
+		}
+	}
+}
+
