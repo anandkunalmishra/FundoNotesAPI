c5d12dcaa4231e2cc23528dac80392e08d5944cf
Anand Kunal Mishra <73222935+anandkunalmishra@users.noreply.github.com>
2024-03-02 07:16:57 +0530

    [Anand]: Lable Table

---
+++ Repository_Layer/Migrations/20240301124903_label.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RepositoryLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class label : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LabelTable",
+                columns: table => new
+                {
+                    LabelId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LabelName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NoteId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LabelTable", x => x.LabelId);
+                    table.ForeignKey(
+                        name: "FK_LabelTable_NotesTable_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "NotesTable",
+                        principalColumn: "NoteId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LabelTable_UserTable_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserTable",
+                        principalColumn: "userId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabelTable_NoteId",
+                table: "LabelTable",
+                column: "NoteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LabelTable_UserId",
+                table: "LabelTable",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LabelTable");
+        }
+    }
+}
---
+++ Repository_Layer/Services/LabelRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Repository_Layer.Services
+{
+	public class LabelRepository
+	{
+		
+	}
+}
+
